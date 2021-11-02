8a8578ee9897efdb7e95b09be6146292a9d228d7
Amat Deska <amatdeska@Amats-MacBook-Pro.local>
2021-11-02 08:46:29 +0700

    Example1

--- resources/views/home.blade.php
+++ resources/views/home.blade.php
@@ -20,16 +20,25 @@
         integrity="sha384-JZR6Spejh4U02d8jOt6vLEHfe/JQGiRRSQQxSfFWpi1MquVdAyjUar5+76PVCmYl" crossorigin="anonymous">
     </script>
 
+    <!-- AOS -->
+    <link href="https://unpkg.com/aos@2.3.1/dist/aos.css" rel="stylesheet">
+    <script src="https://unpkg.com/aos@2.3.1/dist/aos.js"></script>
+
+
     <!-- Font Awesome 4.7 -->
-    <link rel="stylesheet" href="https://cdnjs.cloudflare.com/ajax/libs/font-awesome/4.7.0/css/font-awesome.min.css" integrity="sha512-SfTiTlX6kk+qitfevl/7LibUOeJWlt9rbyDn92a1DqWOw9vWG2MFoays0sgObmWazO5BQPiFucnnEAjpAB+/Sw==" crossorigin="anonymous" referrerpolicy="no-referrer" />
-    <link rel="stylesheet" href="https://cdnjs.cloudflare.com/ajax/libs/font-awesome/4.7.0/css/font-awesome.css" integrity="sha512-5A8nwdMOWrSz20fDsjczgUidUBR8liPYU+WymTZP1lmY9G6Oc7HlZv156XqnsgNUzTyMefFTcsFH/tnJE/+xBg==" crossorigin="anonymous" referrerpolicy="no-referrer" />
+    <link rel="stylesheet" href="https://cdnjs.cloudflare.com/ajax/libs/font-awesome/4.7.0/css/font-awesome.min.css"
+        integrity="sha512-SfTiTlX6kk+qitfevl/7LibUOeJWlt9rbyDn92a1DqWOw9vWG2MFoays0sgObmWazO5BQPiFucnnEAjpAB+/Sw=="
+        crossorigin="anonymous" referrerpolicy="no-referrer" />
+    <link rel="stylesheet" href="https://cdnjs.cloudflare.com/ajax/libs/font-awesome/4.7.0/css/font-awesome.css"
+        integrity="sha512-5A8nwdMOWrSz20fDsjczgUidUBR8liPYU+WymTZP1lmY9G6Oc7HlZv156XqnsgNUzTyMefFTcsFH/tnJE/+xBg=="
+        crossorigin="anonymous" referrerpolicy="no-referrer" />
 
 </head>
 
 <body>
     <!-- Nav  -->
-    <nav class="navbar navbar-expand-lg navbar-dark bg-primary">
-        <a class="navbar-brand" href="#">Navbar</a>
+    <nav class="navbar navbar-expand-lg navbar-dark nav-background">
+        <img src="{{asset('img/user1.jpg')}}" class="icon">
         <button class="navbar-toggler" type="button" data-toggle="collapse" data-target="#navbarSupportedContent"
             aria-controls="navbarSupportedContent" aria-expanded="false" aria-label="Toggle navigation">
             <span class="navbar-toggler-icon"></span>
@@ -60,20 +69,20 @@
                 </li>
             </ul>
             <ul class="navbar-nav">
-                <li class="nav-item active">
+                <li class="nav-item">
                     <a class="nav-link" href="#">
-                    <i class="fa fa-sign-in" aria-hidden="true"></i>
-Login</a>
+                        <i class="fa fa-sign-in" aria-hidden="true"></i>
+                        Login</a>
                 </li>
-                <li class="nav-item active">
+                <!-- <li class="nav-item active">
                     <a class="nav-link">/</a>
-                </li> 
-                <li class="nav-item active">
+                </li> -->
+                <li class="nav-item">
                     <a class="nav-link" href="#">
-                    <i class="fa fa-plus-square" aria-hidden="true"></i>
-Register</a>
+                        <i class="fa fa-plus-square" aria-hidden="true"></i>
+                        Register</a>
                 </li>
-                
+
             </ul>
         </div>
     </nav>
@@ -109,7 +118,7 @@ Register</a>
     <!-- Content -->
     <div class="container container1">
         <div class="row">
-            <div class="col-sm-4 content-line">
+            <div class="col-sm-4 content-line" data-aos="zoom-out-up" data-aos-duration="1000">
                 <div class="card">
                     <div class="card-body">
                         <h5 class="card-title">Special title treatment</h5>
@@ -118,7 +127,7 @@ Register</a>
                     </div>
                 </div>
             </div>
-            <div class="col-sm-4 content-line">
+            <div class="col-sm-4 content-line" data-aos="zoom-out-up" data-aos-duration="1000">
                 <div class="card">
                     <div class="card-body">
                         <h5 class="card-title">Special title treatment</h5>
@@ -127,7 +136,7 @@ Register</a>
                     </div>
                 </div>
             </div>
-            <div class="col-sm-4 content-line">
+            <div class="col-sm-4 content-line" data-aos="zoom-out-up" data-aos-duration="1000">
                 <div class="card">
                     <div class="card-body">
                         <h5 class="card-title">Special title treatment</h5>
@@ -161,14 +170,115 @@ Register</a>
         </div>
     </div>
 
-    <!-- Content End -->
-    <hr class="hr">
+    <!-- Example Text -->
+    <div class="container">
+        <h1 class="content-line title-text">Example Text</h1>
+        <div class="row">
+            <div class="col-md-6">
+                <img src="{{ asset('img/user1.jpg') }}" class="img-radius1" data-aos="zoom-out-right"
+                    data-aos-duration="1000">
+            </div>
+            <div class="col-md-6">
+                <p class="content-text">Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor
+                    incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam, quis nostrud exercitation
+                    ullamco laboris nisi ut aliquip ex ea commodo consequat. Duis aute irure dolor in reprehenderit in
+                    voluptate velit esse cillum dolore eu fugiat nulla pariatur. Excepteur sint occaecat cupidatat non
+                    proident, sunt in culpa qui officia deserunt mollit anim id est laborum.</p>
+            </div>
+        </div>
+    </div>
 
-    <!-- Modal -->
+    <!-- Feature -->
+    <div class="container">
+        <h1 class="content-line title-text">Feature</h1>
+        <div class="row">
+            <div class="col-md-6">
+                <p class="content-text">Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor
+                    incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam, quis nostrud exercitation
+                    ullamco laboris nisi ut aliquip ex ea commodo consequat. Duis aute irure dolor in reprehenderit in
+                    voluptate velit esse cillum dolore eu fugiat nulla pariatur. Excepteur sint occaecat cupidatat non
+                    proident, sunt in culpa qui officia deserunt mollit anim id est laborum.</p>
+            </div>
+            <div class="col-md-6">
+                <img src="{{ asset('img/user1.jpg') }}" class="img-radius1" data-aos="zoom-out-left"
+                    data-aos-duration="1000">
+            </div>
+        </div>
+    </div>
+
+    <!-- Footer -->
+    <footer class="footer">
+        <div class="container">
+            <div class"container1">
+                <div class="row">
+                    <div class="col-sm-4 content-text">
+                        <p class="footer-text">About</p>
+                        <p class="footer-text">The standard chunk of Lorem Ipsum used since the 1500s is reproduced
+                            below for those interested. Sections 1.10.32 and 1.10.33 from "de Finibus Bonorum et
+                            Malorum" by Cicero are also reproduced in their exact original form, accompanied by English
+                            versions from the 1914 translation by H. Rackham.</p>
+                    </div>
+                    <div class="col-sm-4 content-text">
+                        <p class="footer-text">Address</p>
+                        <p class="footer-text"><i class="fa fa-map-marker" aria-hidden="true"></i>
+                    Jl. Karang Tengah, Tangerang Banten
+                    </p>
+                    <p class="#"><i class="fa fa-map-marker" aria-hidden="true"></i>
+                    Indonesia
+                    </p>
+                    <p class="footer-text"><i class="fa fa-compass" aria-hidden="true"></i>
+                Code Pos 15157
+                </p>
+                <a href="mailto:amatdeska97@gmail.com" class="footer-text-email"><i class="fa fa-envelope" aria-hidden="true"></i>
+            amatdeska97@gmail.com
+            </a>
+                    </div>
+                    <div class="col-sm-4 content-text">
+                        <p class="footer-text">Message</p>
+                        <form>
+                            <div class="row">
+                                <div class="col input-footer">
+                                    <input type="text" class="form-control" placeholder="First name">
+                                </div>
+                                <div class="col input-footer">
+                                    <input type="text" class="form-control" placeholder="Last name">
+                                </div>
+                            </div>
+                            <div class="row">
+                                <div class="col-12 input-footer">
+                                    <input type="email" class="form-control" placeholder="Email">
+                                </div>
+                                <div class="col-12 input-footer">
+                                    <textarea class="form-control" id="exampleFormControlTextarea1" rows="3"
+                                        placeholder="Message"></textarea>
+                                </div>
+                            </div>
+                            <div class="row">
+                                <div class="col input-footer">
+                                    <button type="button" class="btn btn-primary">Send</button> </div>
+                            </div>
+                        </form>
+                    </div>
+                    <div class="col-sm-12">
+                        <p class="company">Amat Deska Arya &copy; 2021</p>
+                    </div>
+                </div>
+            </div>
+        </div>
+    </footer>
+
+    <!-- Content End -->
+    <!-- <hr class="hr"> -->
+
+    <!-- Modal JS -->
     <script>
         $('#myModal').on('shown.bs.modal', function () {
             $('#myInput').trigger('focus')
         })
+
+    </script>
+    <script>
+        AOS.init();
 
     </script>
 </body>
